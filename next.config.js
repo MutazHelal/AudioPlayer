c455106a89c6829eeffa8e53f1e71a3a9644ff6b
MutazHelal <helala2@mcmaster.ca>
2022-03-24 14:24:28 -0400

    edits

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 module.exports = {
   reactStrictMode: true,
   typescript: {
-    // ! WARN !!!
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
     ignoreBuildErrors: true,
   },
   eslint: {
-    // ! WARN
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 }
